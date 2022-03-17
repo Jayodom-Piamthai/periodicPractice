7b70cc5bea832c4405ce6db330a91b29ae3ff9ff
CEEXAGON <acejayodom@gmail.com>
2022-03-17 14:04:52 +0700

    a

---
+++ chem-prac-python/Periodic_table.py
@@ -0,0 +1,26 @@
+GroupAList = ['h','li','na','k','rb','cs','fr',
+                'be','mg','ca','sr','ba','ra',
+                'b','al','ga','in','tl','nh',
+                'c','si','ge','sn','pb','fl',
+                'n','p','as','sb','bi','mc',
+                'o','s','se','te','po','lv',
+                'f','cl','br','i','at','ts',
+                'he','ne','ar','kr','xe','rn','og']
+A1 = ['h','li','na','k','rb','cs','fr']
+A2 = ['be','mg','ca','sr','ba','ra']
+A3 = ['b','al','ga','in','tl','nh']
+A4 = ['c','si','ge','sn','pb','fl']
+A5 = ['n','p','as','sb','bi','mc']
+A6 = ['o','s','se','te','po','lv']
+A7 = ['f','cl','br','i','at','ts']
+A8 = ['he','ne','ar','kr','xe','rn','og']
+GroupBList = []
+print (A1)
+
+
+
+
+
+
+
+
